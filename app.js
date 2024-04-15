2a2bbbe9a43d01909b54edc7d421038074f16c3e
IT20636524 <IT20636524@my.sliit.lk>
2024-04-16 01:10:29 +0530

    user functionalities

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+dotenv.config();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const userRouter = require("./routes/users");
+app.use("/api/user", userRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, ()=>{
+    console.log(`Backend is running on port ${PORT}`)
+});
---
+++ controllers/userController.js
@@ -0,0 +1,74 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const sendTokenResponse = async (res, user, message) => {
+  const accessToken = generateToken(user);
+
+  res.status(200).json({
+    data: { user, access_token: accessToken },
+    message,
+  });
+};
+
+const generateToken = (user) => {
+  return jwt.sign({ data: user }, process.env.JWT_SECRET || "jsecret", {
+    expiresIn: `30d`,
+  });
+};
+
+async function generateUserId() {
+  let userNumber = await User.countDocuments({});
+  console.log("Users List", userNumber);
+  return userNumber < 9
+    ? "u_00" + (userNumber + 1)
+    : "u_0" + (userNumber + 1);
+}
+
+const registerUser =async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPass = await bcrypt.hash(req.body.password, salt);
+  const newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashedPass,
+    role: req.body.role,
+  });
+  try {
+    newUser.userId = await generateUserId();
+    try {
+      const user = await newUser.save();
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const loginUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+
+    console.log(req.body.password);
+    if (!user) {
+      res.status(400).json("Wrong credentials");
+    } else {
+      const validated = await bcrypt.compare(req.body.password, user.password);
+      // !validated && res.status(400).json("Wrong credentials")
+      if (!validated) {
+        res.status(400).json("Wrong credentials");
+      } else {
+        const { password, ...others } = user._doc;
+        console.log(others);
+        sendTokenResponse(res, others, "successful");
+        // res.status(200).json(res, others);
+      }
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+};
+
+module.exports = { registerUser, loginUser };
